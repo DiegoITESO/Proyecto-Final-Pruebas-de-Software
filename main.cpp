c41b13f558f6b79bcdb31d2773aa4753590a3713
ELIZAN <eli.j.alvarez@gmail.com>
2026-05-06 20:16:51 -0600

    style: clang-format correction on main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+
 #include "./include/cli/ArgParser.hpp"
+
 using namespace std;
-int main(int argc, char* argv[])
-{
-    ArgParser parser(argc, argv);
-    parser.run();
-    return 0;
+
+int main(int argc, char* argv[]) {
+  ArgParser parser(argc, argv);
+  parser.run();
+  return 0;
 }
